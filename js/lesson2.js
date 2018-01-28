1838aed8af083872ccb8e75867d808733e362c00
Ali Cope <35251289+Cope-Ali@users.noreply.github.com>
2018-01-27 17:24:53 -0800

    bug fix1

--- js/lesson2.js
+++ js/lesson2.js
@@ -13,7 +13,7 @@ var iphonex = {
 //Display galaxyx and iphonex object values
 function displayObject() {
     var output = Object.values(galaxyx) + "<br>" + Object.values(iphonex);
-    document.getElementById(displayObject1).innerHTML = output;
+    document.getElementById(displayObject1).innerHTML=output;
 }
 
 // add a method to display make and model together
@@ -28,7 +28,7 @@ function createMethod() {
 function addMethod() {
     createMethod()
     output = Object.values(galaxyx) + "<br>" + Object.values(iphonex);
-    document.getElementById(methoddiv).innerHTML = output;
+    document.getElementById(methoddiv).innerHTML=output;
 }
 
 // create new phone model using galaxyx as a prototype
@@ -39,7 +39,7 @@ function createPrototype() {
 //display prototype
 function displayObject2() {
     output = Object.values(galaxys9);
-    document.getElementById(prototypediv).innerHTML = output;
+    document.getElementById(prototypediv).innerHTML=output;
 }
 
 //change the value of model to GalaxyS9
@@ -50,11 +50,11 @@ function changeModel() {
 //Display function with changed model
 function displayChangedModel() {
     output = Object.values(galaxys9);
-    document.getElementById(changedmodeldiv).innerHTML = output;
+    document.getElementById(changedmodeldiv).innerHTML=output;
 }
 
 //Display all objects
 function displayAllObjects() {
     output = Object.values(galaxyx) + "<br>" + Object.values(iphonex) + "<br>" + Object.values(galaxys9);
-    document.getElementById(displayalldiv).innerHTML = output;
+    document.getElementById(displayalldiv).innerHTML=output;
 }
