002447e68bc1ea38de8c94e5830687cd036a6901
Ali Cope <35251289+Cope-Ali@users.noreply.github.com>
2018-02-02 11:03:06 -0800

    javascript update lesson 3

--- js/lesson3.js
+++ js/lesson3.js
@@ -25,19 +25,51 @@ var residentObjectB = JSON.parse(residentStringB, function(key,value){
 
 //display JSON object
 function displayParse(){
-    var output = resident
+    var residentObject = {name:"Mary", apt:36};
+
+    //convert to JSON object with JSON.stringify
+    var residentString = JSON.stringify(residentObject);
+
+    //convert from JSON object into JavaScript object with JSON.parse
+    var resident = JSON.parse(residentString);
+
+    var output = resident;
     document.getElementById(parseDiv).innerHTML=output;
 }
 
 //display JavaScript object
 function displayStringify(){
-    var output = residentString
+    //create JavaScript Object
+    var residentObject = {name:"Mary", apt:36};
+
+    //convert to JSON object with JSON.stringify
+    var residentString = JSON.stringify(residentObject);
+
+    var output = residentString;
     document.getElementById(stringifyDiv).innerHTML=output;
 }
 
 //display JavaScript object with date
 function displayDate(){
-    var output = residentObjectB
+    //create JavaScript Object
+    var residentObject = {name:"Mary", apt:36};
+
+    //add date to resident object
+    resident.leaseDate = 2012-10-15;
+
+    //convert JavaScript object back to JSON object
+    var residentStringB = JSON.stringify(resident);
+
+    //convert date from JSON object into date in JavaScript Object
+    var residentObjectB = JSON.parse(residentStringB, function(key,value){
+        if(key=="leaseDate"){
+            return new Date(value);}
+        else{
+            return value;
+        }
+
+    })
+    var output = residentObjectB;
     document.getElementById(dateDiv).innerHTML=output;
 }
 
