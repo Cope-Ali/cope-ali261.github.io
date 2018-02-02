fd3b05dc4a761a97322e742f58e6928b32317b5f
Ali Cope <35251289+Cope-Ali@users.noreply.github.com>
2018-02-02 11:33:53 -0800

    Update lesson3.js

--- js/lesson3.js
+++ js/lesson3.js
@@ -1,45 +1,31 @@
-//create JavaScript Object
-var residentObject = {
-    name: "Mary",
-    apt: 36
-};
-
-//convert to JSON object with JSON.stringify
-var residentString = JSON.stringify(residentObject);
-
-//convert from JSON object into JavaScript object with JSON.parse
-var resident = JSON.parse(residentString);
-
-//add date to resident object
-resident.leaseDate = 2012 - 10 - 15;
-
-//convert JavaScript object back to JSON object
-var residentStringB = JSON.stringify(resident);
-
-//convert date from JSON object into date in JavaScript Object
-var residentObjectB = JSON.parse(residentStringB, function (key, value) {
-    if (key == "leaseDate") {
-        return new Date(value);
-    } else {
-        return value;
-    }
-
-})
 
 //display JSON object
 function displayParse() {
+    var residentString = '{"name":"Mary", "apt":36}';
+    var resident = JSON.parse(residentString);
     var output = resident;
     document.getElementById("parseDiv").innerHTML = output;
 }
 
 //display JavaScript object
 function displayStringify() {
+    var residentObject = {name:"Mary", apt:36};
+
+//convert to JSON object with JSON.stringify
+    var residentString = JSON.stringify(residentObject);
     var output = residentString;
     document.getElementById("stringifyDiv").innerHTML = output;
 }
 
 //display JavaScript object with date
 function displayDate() {
+    var residentString = '{"name":"Mary", "apt":36, "leaseDate":"2012-10-15"}';
+    var residentObjectB = JSON.parse(residentStringB, function (key, value) {
+    if (key == "leaseDate") {
+        return new Date(value);} 
+        else {
+        return value;}
+})
     var output = residentObjectB;
     document.getElementById("dateDiv").innerHTML = output;
 }
