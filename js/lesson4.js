2567a4e7e7218748d197686f857538d49765dabb
Ali Cope <35251289+Cope-Ali@users.noreply.github.com>
2018-02-05 20:51:22 -0800

    lesson 4

---
+++ js/lesson4.js
@@ -0,0 +1,14 @@
+function requestDisplay(){
+var xhttp = new XMLHttpRequest();
+    var results = document.getElementById('displayDiv');
+xhttp.onreadystatechange = function (){
+    if (this.readyState == 4 && this.status == 200){
+        var data = JSON.parse(r.responseText);
+        for (i=0;i<data.length;i++){
+            results.innerHTML += '<li>'+data[i].f+'</li>'
+        }
+    }
+}
+r.open("GET", "http://www.filltext.com/?rows=10&firstname={firstName}&lastname={lastName}&email={email}&pretty=true", true);
+    r.send();
+};
