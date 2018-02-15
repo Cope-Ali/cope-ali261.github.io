659c93264ae37d7abe9fe8e2b13262ba8826a066
Ali Cope <35251289+Cope-Ali@users.noreply.github.com>
2018-02-15 11:28:57 -0800

    update and add lesson 7

---
+++ js/lesson7.js
@@ -0,0 +1,19 @@
+//hide and unhide example text
+function hideText(){
+    document.getElementById("exampleText").classList.toggle("hide");
+}
+
+// change color from default to blue
+function colorText(){
+    document.getElementById("exampleText").classList.toggle("color");
+}
+
+// move cat from left to right
+function moveCat(){
+    document.getElementById("cat").classList.toggle("right");
+}
+
+//hide and unhide cat
+function hideCat(){
+    document.getElementById("cat").classList.toggle("hideCat")
+}
