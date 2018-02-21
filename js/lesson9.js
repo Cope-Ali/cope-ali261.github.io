7a826585853ff726aa21c7cb88186388f4b11dd2
Ali Cope <35251289+Cope-Ali@users.noreply.github.com>
2018-02-20 20:41:51 -0800

    sperate js scripts

--- js/lesson9.js
+++ js/lesson9.js
@@ -8,12 +8,3 @@ function mouse() {
     console.log("mouse");
 }
 
-document.querySelector('#mobile')addEventListener('touchstart', touchFunction,false);
-
-// what happens when touchstart
-function touchFunction(ev) {
-    console.log(ev.touches);
-    var pageX = ev.touches.pageY;
-    var pageY = ev.touches.pageX;
-    document.getElementById("outputDiv").innerHTML = "You are seeing this because you triggered a touch event!" + "X position: " + pageX + "Y position: " + pageY;
-}
